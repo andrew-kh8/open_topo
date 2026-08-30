c189521ba7baac70ff23599c78ee1f0e768af4c2
andrew-kh8 <horolskyandrey@gmail.com>
2026-08-30 22:06:01 +0300

    request catalog spec

--- spec/open_topo/net/request_spec.rb
+++ spec/open_topo/net/request_spec.rb
@@ -86,4 +86,77 @@ RSpec.describe OpenTopo::Net::Request do
       end
     end
   end
+
+  describe "#catalog" do
+    subject(:catalog) { instance.catalog(params) }
+
+    context "with 4 coords params" do
+      let(:params) { build(:catalog_params, west:, south:, east:, north:) }
+      let(:north) { 84.5 }
+      let(:south) { 84 }
+      let(:west) { 121 }
+      let(:east) { 121.5 }
+
+      it "returns an array of Catalog" do
+        VCR.use_cassette("catalog/success") do
+          expect(catalog).to be_a(OpenTopo::Net::Response)
+          expect(catalog.status).to eq 200
+          expect(catalog.body).to be_a(Hash)
+        end
+      end
+
+      context "when param is invalid" do
+        let(:east) { west }
+
+        it "raises an error" do
+          VCR.use_cassette("catalog/failure_invalid_params") do
+            expect(catalog).to be_a(OpenTopo::Net::Response)
+            expect(catalog.status).to eq 400
+            expect(catalog.body).to be_a(Hash)
+          end
+        end
+      end
+
+      context "when there is no data" do
+        let(:north) { 89.5 }
+        let(:south) { 89 }
+
+        it "returns a response" do
+          VCR.use_cassette("catalog/success_no_data") do
+            expect(catalog).to be_a(OpenTopo::Net::Response)
+            expect(catalog.status).to eq 200
+            expect(catalog.body).to be_a(Hash)
+          end
+        end
+      end
+    end
+
+    context "with polygon params" do
+      let(:params) { build(:catalog_params, polygon:) }
+
+      context "with valid polygon" do
+        let(:polygon) { "-117.5,32.5,-117.5,33.1,-116.7,33.1,-116.7,32.5,-117.0,32.3,-117.5,32.5" }
+
+        it "returns array of Catalog" do
+          VCR.use_cassette("catalog/polygon_success") do
+            expect(catalog).to be_a(OpenTopo::Net::Response)
+            expect(catalog.status).to eq 200
+            expect(catalog.body).to be_a(Hash)
+          end
+        end
+      end
+
+      context "with invalid polygon" do
+        let(:polygon) { "-117.5,32.5,-117.0,32.3,-117.5,32.5" }
+
+        it "returns array of Catalog" do
+          VCR.use_cassette("catalog/polygon_failure") do
+            expect(catalog).to be_a(OpenTopo::Net::Response)
+            expect(catalog.status).to eq 400
+            expect(catalog.body).to be_a(Hash)
+          end
+        end
+      end
+    end
+  end
 end
